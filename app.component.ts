54cb8eacfd4837f70a8885336bd2f5161f559ce2
Rodrigo Marques <judemarques8@gmail.com>
2026-06-23 03:05:17 -0300

    Refactor AppComponent to be standalone

--- app.component.ts
+++ app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgModule } from '@angular/core';
-
 
 @Component({
   selector: 'app-root',
-  standalone: false,
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'livros-angular';
-}
+export class AppComponent {}
